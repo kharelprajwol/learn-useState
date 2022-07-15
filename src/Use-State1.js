3661a3dbf66d5331e5e160221d834d3c276b1113
Prajwol Kharel <xyzkharelp@gmail.com>
2022-07-15 11:59:22 +0545

    Notes are added

--- src/Use-State1.js
+++ src/Use-State1.js
@@ -1,28 +1,39 @@
-import {React, useState} from 'react';
+
+// Importing useState
+import {useState} from 'react';
 
 
 function UseStateDemo1() {
+
+    // Using useState
     const [count, setCount] = useState(0);
+    // In above line: 
+    // -- The initial value of state is set to 0;
+    // -- Since useState return array, that array is destructed.
+    //    -- the first value, which is the current state is assigned to a variable named count.
+    //    -- the second value, which is the function that updates the value of count is named as, setCount
 
-    function increaseCount() {
-        setCount(count+1);
+
+    function increaseCount() { // invoked when user press Increase button
+        setCount(count+1); // current state's value is increased by one.
     }
 
-    function decreaseCount() {
-        setCount(count-1);
+    function decreaseCount() { // invoked when user press Decrease button
+        setCount(count-1); // current state's value is decreased by one.
     }
 
-    function resetCount() {
-        setCount(0);
+    function resetCount() { // invoked when user press Reset button
+        setCount(0); // current state's value is set to zero.
     }
-
-
-
 
     return (
         <div>
             <h1>useState demo 1</h1>
-            <h3>Count: {count}</h3>
+            <h3>Count: {count}</h3> {/*displaying current value of state */}
+
+            {/* Using event handler in button tag:
+              * always pass function in event handler.
+              */}
             <button onClick={increaseCount}>Increase</button>
             <button onClick={decreaseCount}>Decrease</button>
             <button onClick={resetCount}>Reset</button>
@@ -30,4 +41,4 @@ function UseStateDemo1() {
     );
 }
 
-export default UseStateDemo1;
+export default UseStateDemo1; {/*Exporting this component */}
--- src/Use-State2.js
+++ src/Use-State2.js
@@ -3,16 +3,24 @@ import {React,useState} from 'react';
 
 function UseStateDemo2() {
 
-    const [text,setText] = useState('');
-    function handleInput(e) {
-        setText(e.target.value);
+    {/* Using useState:
+      * passing initial value to useSate.
+      * the return value of useState is destructured. 
+      */}
+    const [text,setText] = useState(''); 
+
+    // Function to handle change in input field.
+    function handleInput(e) { // invoked when there is a change in input field.
+        setText(e.target.value); // the changed (updated or new) value is set to the current value of state.
 
     }
     return(
         <div>
             <h1>useState demo 2</h1>
+
+            {/*Using event handler in input field. */}
             <input type="text" placeHolder="Type here" onChange={handleInput}></input>
-            <h3>Your Text: {text}</h3>
+            <h3>Your Text: {text}</h3> {/*Displaying the current value of state */}
         </div>
     )
 }
